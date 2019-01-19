5f391fa854db443acb657a837fa6f5d77a5f14ff
Volodymyr <klymenkodp@gmail.com>
2019-01-19 16:25:43 -0500

    Add Car Info column

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -20,13 +20,46 @@ export default ({ info, odometer }) => (
       </div>
     </div>
     <div className="dashboard">
-      <button
+      <div className="left-col">
+        <div className="car-info">
+          <p className="car-header">
+            You own a <b>{info.model}</b>
+          </p>
+          <img
+            className="car-avatar"
+            src="https://insideevs.com/wp-content/uploads/2017/12/IMG_20171214_164551-e1529516907468.jpg"
+            alt="Car image"
+          />
+
+          <table className="car-metrics">
+            <tr>
+              <td className="metric-title">
+                <b>Mileage</b>
+              </td>
+              <td className="metric">{info.odometer.data} km</td>
+            </tr>
+            <tr>
+              <td className="metric-title">
+                <b>Current location</b>
+              </td>
+              <td className="metric">24 Valley Dr., CA</td>
+            </tr>
+            <tr>
+              <td className="metric-title">
+                <b>Estimated Trade-in Value</b>
+              </td>
+              <td className="metric">$ 10,000</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      {/* <button
         onClick={() => {
           console.log(odometer);
         }}
       >
         Odometr
-      </button>
+      </button> */}
     </div>
   </div>
 );
