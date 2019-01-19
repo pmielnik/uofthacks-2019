de94706c5524b294883996fde8f600e9826a7489
Miranda Harrison <mirandaharrison18@gmail.com>
2019-01-19 11:17:03 -0500

    reverted app.js to default

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,66 +1,26 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Smartcar from "@smartcar/auth";
-
-import Connect from "./components/Connect";
-import Vehicle from "./components/Vehicle";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      vehicle: {},
-      odometer: {}
-    };
-
-    this.authorize = this.authorize.bind(this);
-
-    this.onComplete = this.onComplete.bind(this);
-    this.smartcar = new Smartcar({
-      clientId: process.env.REACT_APP_CLIENT_ID,
-      redirectUri: process.env.REACT_APP_REDIRECT_URI,
-      scope: [
-        "read_vehicle_info",
-        "read_odometer",
-        "read_location",
-        "read_vin"
-      ],
-      testMode: true,
-      onComplete: this.onComplete
-    });
-  }
-
-  onComplete(err, code, status) {
-    return axios
-      .get(`${process.env.REACT_APP_SERVER}/exchange?code=${code}`)
-      .then(_ => {
-        return axios.get(`${process.env.REACT_APP_SERVER}/vehicle`);
-      })
-      .then(res => {
-        this.setState({ vehicle: res.data });
-      })
-      .then(_ => {
-        return axios.get(
-          `${process.env.REACT_APP_SERVER}/odometer?vehicleId=${
-            this.state.vehicle.id
-          }`
-        );
-      })
-      .then(res => {
-        this.setState({ odometer: res.data });
-      });
-  }
-
-  authorize() {
-    this.smartcar.openDialog({ forcePrompt: true });
-  }
-
   render() {
-    return Object.keys(this.state.vehicle).length !== 0 ? (
-      <Vehicle info={this.state.vehicle} odometer={this.state.odometer} />
-    ) : (
-      <Connect onClick={this.authorize} />
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
